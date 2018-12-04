home-assistant/core

Add Hue sensor support
Bump aiohue to 1.6.0

homeassistant/components/hue/bridge.py
@@ -78,8 +78,9 @@ class HueBridge(object):
                              host)
             return False
 
-        hass.async_add_job(hass.config_entries.async_forward_entry_setup(
-            self.config_entry, 'light'))
+        for platform in ['light', 'sensor']:
+            hass.async_add_job(hass.config_entries.async_forward_entry_setup(
+                self.config_entry, platform))
 
         hass.services.async_register(
             DOMAIN, SERVICE_HUE_SCENE, self.hue_activate_scene,

pmaciocia/home-assistant

homeassistant/components/hue/bridge.py
@@ -79,8 +79,9 @@ class HueBridge:
                              host)
             return False
 
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(
-            self.config_entry, 'light'))
+        for platform in ['light', 'sensor']:
+            hass.async_create_task(hass.config_entries.async_forward_entry_setup(
+                self.config_entry, platform))
 
         hass.services.async_register(
             DOMAIN, SERVICE_HUE_SCENE, self.hue_activate_scene,